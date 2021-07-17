c19f707037c201dbfca4869b3d17bde660353531
iamcosta <yannzyto@gmail.com>
2021-07-17 15:23:30 -0400

    prototype hook and showcase

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+//import AppPrototype from './AppPrototype';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/* <AppPrototype /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
